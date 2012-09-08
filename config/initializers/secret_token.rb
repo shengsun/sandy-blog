2a1018412bb40259d8ca224e4165071f002f5b1f
U-ADMINIB-5MCICOP\suns <suns@ADMINIB-5MCICOP.(none)>
2012-09-08 13:14:40 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PostgresTest::Application.config.secret_token = '5e41769ef141d2cf6204e5b7b56ce607f4bfdee8193f7cdd16f2110a4e96eed83f57ea765f059c6729f8e29eb33da1baa17bf19dd114a3397aba286ac6edc496'
